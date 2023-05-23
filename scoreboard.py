eca539e4e43ae85b1cdd2847ad13c5e1a5444c53
Qubav <124883831+Qubav@users.noreply.github.com>
2023-05-23 15:33:48 +0200

    adding docstrins

--- scoreboard.py
+++ scoreboard.py
@@ -19,16 +19,17 @@ class ScoreBoard(Turtle):
         self.update_scoreboard()
     
     def update_scoreboard(self):
-        """Function writes player score after it clears old score."""
+        """Method writes player score after it clears old score."""
         self.clear()
         self.write(f"Score: {self.score}   Highest Score: {self.highest_score}", align = ALIGNMENT, font = FONT)
 
     def increase_score(self):
-        """Function increases player score and updates score board."""
+        """Method increases player score and updates score board."""
         self.score += 1
         self.update_scoreboard()
 
     def reset(self):
+        """Method checks if score from previous game has has higher value than highest score and updates it if this criteria is met, and resets player score to 0 and updates scoreboard."""
 
         if self.score > self.highest_score:
 
@@ -47,6 +48,7 @@ class ScoreBoard(Turtle):
         self.highest_score = value
     
     def update_highest_score_data(self):
+        """Updating data file with new highest score value."""
 
         with open("data.txt", mode = "w") as file:
             file.write(f"{self.highest_score}")
